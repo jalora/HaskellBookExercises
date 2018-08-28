94837c4d1b92a3a4b9192f1def990a1028df9221
John Alora <jalora@mit.edu>
2018-08-27 22:17:56 -0500

    chp 11 work

--- Chapter 1-11/ch11.hs
+++ Chapter 1-11/ch11.hs
@@ -29,3 +29,40 @@ instance TooMany (Int, String) where
 --3)
 instance (Num a, TooMany a) => TooMany (a, a) where
  tooMany (x, y) = tooMany (x + y)
+
+-- Exercise: Programmers
+data OperatingSystem =
+      GnuPlusLinux
+    | OpenBSDPlusNevermindJustBSDStill
+    | Mac
+    | Windows
+    deriving (Eq, Show)
+
+data ProgLang =
+      Haskell
+    | Agda
+    | Idris
+    | Purescript
+    deriving (Eq, Show)
+
+data Programmer =
+ Programmer { os :: OperatingSystem
+            , lang :: ProgLang }
+ deriving (Eq, Show)
+
+allOperatingSystems :: [OperatingSystem]
+allOperatingSystems =
+  [ GnuPlusLinux
+  , OpenBSDPlusNevermindJustBSDStill
+  , Mac
+  , Windows
+  ]
+
+allLanguages :: [ProgLang]
+allLanguages =
+   [Haskell, Agda, Idris, Purescript]
+
+allProgrammers :: [Programmer]
+allProgrammers = [Programmer x y
+                   | x <- allOperatingSystems
+                   , y <- allLanguages]
